99af3692391653efd543c249d2ca86d534ab2898
Abhimanyu <b22bb001@iitj.ac.in>
2025-05-20 13:29:50 +0530

    changes

--- 01/app.js
+++ 01/app.js
@@ -1,4 +1,4 @@
-const element = React.createElement("h1", {color:"Red"}, "Hello");
+const element = React.createElement("h1",{style: {backgroundColor:"Red"}}, "Hello");
 const ReactRoot = ReactDOM.createRoot(document.getElementById("root"));
 
 ReactRoot.render(element);
