8eb253ae56857d4b91038681d382f92f388438ac
Abhimanyu <b22bb001@iitj.ac.in>
2025-05-20 22:29:08 +0530

    ok

--- 02/app.js
+++ 02/app.js
@@ -1,5 +1,8 @@
-const element1 = React.createElement("h1",{style: {backgroundColor:"Red"}}, "Hello");
-const element2 = React.createElement("h1",{style: {backgroundColor:"Red"}}, "Hi");
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const element1 = React.createElement("h1",{key:"h1", style: {backgroundColor:"Red"}}, "Hello");
+const element2 = React.createElement("h1",{key:"h2", style: {backgroundColor:"Red"}}, "Hi");
 
 const div1 = React.createElement("div", {className: "container", id: "d"}, [element1, element2])
 const ReactRoot = ReactDOM.createRoot(document.getElementById("root"))
